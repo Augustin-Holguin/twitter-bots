32541d60b7062831e7ec734c7c00020bb5e7b857
Augustin Holguín <augustin.holguin@gmail.com>
2017-07-12 18:15:25 +0200

    Add files via upload

---
+++ firstbots.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require "twitter"
+
+client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = "m4QO74424q8YHJQyWxPU5fEG1"
+  config.consumer_secret     = "640fXCxV7Nw4hATeFc5v6VNmJzmZavoX50FbkEBlM0BgpvvKvb"
+  config.access_token        = "885126648730988545-lSK6BVtNzsimxYckn6CzuhWZ0M5FrP9"
+  config.access_token_secret = "eUnNqXg2eHFEMmyvdv9UK5rY6KKPnG2ODvnSrQPBB4qu2"
+end
+
+topics = ["gmail.com"]
+client.filter(track: topics.join(",")) do |object|
+  puts object.text if object.is_a?(Twitter::Tweet)
+end
