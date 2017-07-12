469bc62015b40a65e0c9628f5ee21f5aba4549c2
Augustin Holguín <augustin.holguin@gmail.com>
2017-07-12 18:13:23 +0200

    firstbots.rb

--- firstbots.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-require "twitter"
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "m4QO74424q8YHJQyWxPU5fEG1"
-  config.consumer_secret     = "640fXCxV7Nw4hATeFc5v6VNmJzmZavoX50FbkEBlM0BgpvvKvb"
-  config.access_token        = "885126648730988545-lSK6BVtNzsimxYckn6CzuhWZ0M5FrP9"
-  config.access_token_secret = "eUnNqXg2eHFEMmyvdv9UK5rY6KKPnG2ODvnSrQPBB4qu2"
-end
